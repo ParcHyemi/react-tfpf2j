5937187194cb30e805412aca2e817389355e95be
ParcHyemi <86905167+ParcHyemi@users.noreply.github.com>
2023-01-31 10:55:42 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-tfpf2j
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-tfpf2j)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import "antd/dist/antd.css";
+import { List, Avatar, Alert } from "antd";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
